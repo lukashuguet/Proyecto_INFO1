4cf1e731d7832c1292024813b18fe6ccc023929e
lukashuguet <lukas.huguet@estudiantat.upc.edu>
2026-04-14 19:15:59 +0200

    Paso 2

--- airport.py
+++ airport.py
@@ -5,26 +5,27 @@ class Airport:
         self.longitude = longitude
         self.schengen = False
 
-    def IsSchengenAirport(code):
-        if code == ""
-            schengen = False
-            return schengen
-        schengen_codes = [ 'LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH',
+def IsSchengenAirport(code):
+    if code == "":
+        schengen = False
+        return schengen
+    schengen_codes = [ 'LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH',
 'BI','LI', 'EV', 'EY', 'EL', 'LM', 'EN', 'EP', 'LP', 'LZ', 'LJ', 'LE', 'ES',
 'LS']
-        prefix = code[0:2]
-        if prefix in schengen_codes:
-            schengen = True
-            return schengen
-        else:
-            return False
+    prefix = code[0:2]
+    if prefix in schengen_codes:
+        schengen = True
+        return schengen
+    else:
+        schengen = False
+        return schengen
 
-    def SetSchengen(airport):
-        status = IsSchengenAirport(airport.code)
-        airport.schengen = status
+def SetSchengen(airport):
+    status = IsSchengenAirport(airport.code)
+    airport.schengen = status
 
-    def PrintAirport(airport):
-        print("Código ICAO:", airport.code)
-        print("Latitud:", airport.lat)
-        print("Longitud:", airport.lon)
-        print("Schengen:", airport.schengen)
+def PrintAirport(airport):
+    print("Código ICAO:", airport.code)
+    print("Latitud:", airport.latitude)
+    print("Longitud:", airport.longitude)
+    print("Schengen:", airport.schengen)
---
+++ test_airport.py
@@ -0,0 +1,4 @@
+from airport import *
+airport = Airport ("AUBH", 41.26578345, 65.84692941)
+SetSchengen (airport)
+PrintAirport (airport)
