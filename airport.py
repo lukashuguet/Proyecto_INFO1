a79332782d35ff575e0664c6b5f227c99a7fb7e1
lukashuguet <lukas.huguet@estudiantat.upc.edu>
2026-04-14 18:38:45 +0200

    Paso 1

---
+++ airport.py
@@ -0,0 +1,30 @@
+class Airport:
+    def __init__(self, code, latitude, longitude):
+        self.code = code
+        self.latitude = latitude
+        self.longitude = longitude
+        self.schengen = False
+
+    def IsSchengenAirport(code):
+        if code == ""
+            schengen = False
+            return schengen
+        schengen_codes = [ 'LO', 'EB', 'LK', 'LC', 'EK', 'EE', 'EF', 'LF', 'ED', 'LG', 'EH', 'LH',
+'BI','LI', 'EV', 'EY', 'EL', 'LM', 'EN', 'EP', 'LP', 'LZ', 'LJ', 'LE', 'ES',
+'LS']
+        prefix = code[0:2]
+        if prefix in schengen_codes:
+            schengen = True
+            return schengen
+        else:
+            return False
+
+    def SetSchengen(airport):
+        status = IsSchengenAirport(airport.code)
+        airport.schengen = status
+
+    def PrintAirport(airport):
+        print("Código ICAO:", airport.code)
+        print("Latitud:", airport.lat)
+        print("Longitud:", airport.lon)
+        print("Schengen:", airport.schengen)
